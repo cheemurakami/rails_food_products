3ffcbf5a627342a0c1ce198c9e2613ec7538ebb7
Chisato Murakami <kiwipom1031@gmail.com>
2020-05-29 12:51:20 -0700

    pass an additional validation test for rating in review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,14 @@
 class Review < ApplicationRecord
+  
   belongs_to :product
+
   validates :author, presence: true
+
   validates :rating, presence: true
-  validates :content_body, length: {in: 50..250, message: "Contents must be between 50 and 250 characters"}, presence: true
+  
+  validates_numericality_of :rating, greater_than: 0, less_than: 6, message: "Please rate from 1 to 5"
+
+  validates :content_body,  presence: true, length: {in: 50..250, message: "Contents must be between 50 and 250 characters"}
  
 end
 
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 describe Review do
+
   it { should belong_to(:product) }
   it { should validate_presence_of :author}
   it { should validate_presence_of :content_body}
-  it { should validate_presence_of :rating}
   it { should validate_length_of(:content_body).
     is_at_least(50).
     is_at_most(250).
     with_message("Contents must be between 50 and 250 characters")}
-end
+  it { should validate_presence_of :rating}
+  it { should validate_numericality_of(:rating).
+    is_greater_than(0).
+    is_less_than(6).
+    with_message("Please rate from 1 to 5")}
+    
+  end
