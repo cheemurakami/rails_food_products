849bea2b19503d24c047eece886f8f4fe23218ca
Chisato Murakami <kiwipom1031@gmail.com>
2020-05-29 11:08:00 -0700

    add edit page with text_area for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -25,7 +25,22 @@ class ReviewsController < ApplicationController
   def edit
     @product = Product.find(params[:product_id])
     @review = Review.find(params[:id])
-    render :show
+    render :edit
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
   end
 
   private
